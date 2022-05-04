b3c77c7a27bf5e2d439283d33acb486d5d7713c8
Sihoon <tlgns1212@naver.com>
2022-05-04 19:27:18 +0900

    1213 added annotations for blog

--- Baekjoon/1213.py
+++ Baekjoon/1213.py
@@ -1,25 +1,25 @@
 import sys
-word = sys.stdin.readline().strip()
-alphas = [0 for _ in range(27)]
-odds = 0
-oddIndex = -1
+word = sys.stdin.readline().strip()     # 받는 문자열
+alphas = [0 for _ in range(27)]         # 알파벳 개수
+odds = 0                                # 홀수 개수
+oddIndex = -1                           # 홀수의 위치
 for letters in word:
     alphas[ord(letters)-65] += 1
 for i in range(len(alphas)):
     if alphas[i] % 2 == 1:
         odds += 1
         oddIndex = i
-        if odds == 2:
+        if odds == 2:                   # 홀수가 2개 이상이면
             print("I'm Sorry Hansoo")
             exit()
-alphas[oddIndex] -= 1
-for i in range(len(alphas)):
+alphas[oddIndex] -= 1                   # 홀수의 알파벳 하나 미리 빼놓기
+for i in range(len(alphas)):            # 팰린드롬의 좌측 출력
     if alphas[i] % 2 != 1:
         for _ in range(alphas[i]//2):
             print(chr(i+65), end="")
 if oddIndex != -1:
-    print(chr(oddIndex+65), end="")
-for i in range(len(alphas)-1, -1, -1):
+    print(chr(oddIndex+65), end="")     # 미리 빼놓은 알파벳 하나 출력
+for i in range(len(alphas)-1, -1, -1):  # 팰린드롬의 우측 출력
     if alphas[i] % 2 != 1:
         for _ in range(alphas[i]//2):
             print(chr(i+65), end="")
